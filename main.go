14391d642be5b2dc0e7182c654d84e7fc5c871c5
rholdorp <ralph.holdorp@gmail.com>
2022-04-20 18:15:14 +0200

    added http event and response

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"net/http"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"go.uber.org/zap"
 )
@@ -19,9 +21,25 @@ type Event struct {
 	Name string `json:"name"`
 }
 
-func MyHandler(ctx context.Context, e Event) error {
-	logger.Info("in my handler", zap.Any("event", e))
-	return nil
+func MyHandler(ctx context.Context, event events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	//	logger.Info("in my handler", zap.Any("event", e))
+	var res *events.APIGatewayProxyResponse
+
+	logger.Info("recieved event", zap.Any("method", event.HTTPMethod), zap.Any("path", event.Path), zap.Any("body", event.Body))
+
+	if event.Path == "/hello" {
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: http.StatusOK,
+			Body:       "hello",
+		}
+	} else {
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: http.StatusOK,
+			Body:       "default",
+		}
+	}
+
+	return res, nil
 }
 
 func main() {
